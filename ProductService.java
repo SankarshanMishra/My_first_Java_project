d9c8b2a288273a72a9bf4507a39c88a7fc504bfa
SANKARSHAN MISHRA <124585946+SankarshanMishra@users.noreply.github.com>
2024-03-30 01:14:53 +0530

    Update ProductService.java

--- ProductService.java
+++ ProductService.java
@@ -1,37 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class ProductService{
+public class ProductService {
     List<Product> products = new ArrayList<>();
 
-    public void addProduct(Product p){
+    public void addProduct(Product p) {
         products.add(p);
     }
-    public List<Product> getAllProducts(){
+
+    public List<Product> getAllProducts() {
         return products;
     }
-    public Product getProduct(String name){
-        for(Product p : products){
-            if(p.getName().equals(name))
-                return 0;
+
+    public Product getProduct(String name) {
+        for (Product p : products) {
+            if (p.getName().equals(name))
+                return p;
         }
         return null;
     }
-     public List<Product> getAllProductWithText(String black){
-            String str = text.toLowercase();
-           List<Product> prods = new ArrayList<>();
 
-           for(Product p : products){
-                String name = p.getName().toLowercase();
-                String type = p.getType().toLowercase();
-                String place = p.getPlace().toLowercase();
-                if(name.contains(str)|| type.contains(str)|| place.contains(str))
-                    prods.add(p);
-           }
-           return prods;
-       }
-    
-        
-     
+    public List<Product> getAllProductWithText(String text) {
+        String str = text.toLowerCase();
+        List<Product> prods = new ArrayList<>();
+
+        for (Product p : products) {
+            String name = p.getName().toLowerCase();
+            String type = p.getType().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+            if (name.contains(str) || type.contains(str) || place.contains(str))
+                prods.add(p);
+        }
+        return prods;
+    }
 }
